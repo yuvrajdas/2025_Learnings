4e2a6e7079d54048ca2f149ad5896111a8cde268
yuvrajdas <yuvrajdas1531@gmail.com>
2025-06-07 11:47:07 +0530

    DSA learnings

---
+++ DSA/binary-tree/__3ContructBinaryTreeFromInorderAndPostOrderTraversal.java
@@ -0,0 +1,19 @@
+public class __3ContructBinaryTreeFromInorderAndPostOrderTraversal{
+    
+    public static class Node{
+        int data;
+        Node left;
+        Node rigth;
+
+        public Node(){
+
+        }
+        public Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
